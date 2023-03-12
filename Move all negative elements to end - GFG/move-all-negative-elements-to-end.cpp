53330b35b23c47af280b55c3fff23d31cdbd0d31
anirudhshrivastava <84671764+anirudhshrivastava@users.noreply.github.com>
2023-03-12 20:21:49 +0530

    Added solution - LeetHub

--- Move all negative elements to end - GFG/move-all-negative-elements-to-end.cpp
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.cpp
@@ -16,8 +16,8 @@ class Solution{
                 temp[j++]=arr[i];
             }
         }
-        if(j==n || j==0)
-        return;
+        //if(j==n || j==0)
+        //return;
         for(int i=0;i<n;i++){
             if(arr[i]<0){
                 temp[j++]=arr[i];
