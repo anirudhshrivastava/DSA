baba99494f298f06765479899864dc6075168c56
anirudhshrivastava <84671764+anirudhshrivastava@users.noreply.github.com>
2023-09-09 17:19:17 +0530

    Added solution - LeetHub

---
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+double MedianOfArrays(vector<int>& array1, vector<int>& array2);
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    double MedianOfArrays(vector<int>& nums1, vector<int>& nums2)
+    {
+        // Your code goes here
+      /*  vector<int> v;
+        for(auto num :array1){
+            v.push_back(num);
+        }
+        for(auto num : array2){
+            v.push_back(num);
+        }
+        sort(v.begin(),v.end());
+        int n=v.size();
+        
+        double c= n%2 ? v[n/2]:(v[n/2]+v[n/2 -1])/2.0;
+        return c;
+         double flag=0;
+        nums1.insert(nums1.end(),nums2.begin(),nums2.end());
+        sort(nums1.begin(),nums1.end());      
+        if(nums1.size() % 2==0) 
+            return (nums1[nums1.size()/2]+nums1[(nums1.size()/2)-1])/2.0;
+        return nums1[nums1.size()/2];
+    */
+    
+      int m = nums1.size();
+        int n = nums2.size();
+        
+        if (m > n) {
+            return MedianOfArrays(nums2, nums1);
+        }
+        
+        int total = m + n;
+        
+        int low = 0;
+        int high = m;
+        
+        double result = 0.0;
+        
+        while (low <= high) {
+            // nums1
+            int i = low + (high - low) / 2;
+            // nums2
+            int j = (total + 1) / 2 - i;
+            
+            int left1 = (i > 0) ? nums1[i - 1] : INT_MIN;
+            int right1 = (i < m) ? nums1[i] : INT_MAX;
+            int left2 = (j > 0) ? nums2[j - 1] : INT_MIN;
+            int right2 = (j < n) ? nums2[j] : INT_MAX;
+            
+            // partition is correct
+            if (left1 <= right2 && left2 <= right1) {
+                // even
+                if (total % 2 == 0) {
+                    result = (max(left1, left2) + min(right1, right2)) / 2.0;
+                // odd
+                } else {
+                    result = max(left1, left2);
+                }
+                break;
+            } else if (left1 > right2) {
+                high = i - 1;
+            } else {
+                low = i + 1;
+            }
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        cin>>n;
+        vector<int> array2(n);
+        for (int i = 0; i < n; ++i){
+            cin>>array2[i];
+        }
+        Solution ob;
+        cout<<ob.MedianOfArrays(array1, array2)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
